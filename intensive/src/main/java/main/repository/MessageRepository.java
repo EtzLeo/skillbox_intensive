080a5b278ecb6e5e9adc25be17f7d09ebb9f1c04
Kseniia <kbasloviak@gmail.com>
2021-05-29 01:27:02 +0300

    message repository added

--- intensive/src/main/java/main/repository/MessageRepository.java
+++ intensive/src/main/java/main/repository/MessageRepository.java
@@ -3,5 +3,8 @@ package main.repository;
 import main.model.Message;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Репозиторий для доступа к таблице с данными о сообщениях.
+ */
 public interface MessageRepository extends CrudRepository<Message, Integer> {
 }
